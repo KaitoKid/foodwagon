82058ecaac5eec2ed93cf4fcb83bbe0704a0a0d9
Jimmy Yan <yjimmyy@gmail.com>
2016-09-10 21:06:28 -0700

    fix stuff

--- index.js
+++ index.js
@@ -11,6 +11,14 @@ function getId() {
     return wagonId++;
 }
 
+var restaurants = [];
+restaurants.push('McDonalds');
+function pickRandRestaurant() {
+    var restaurant = restaurants[Math.floor(Math.random() * restaurants.length)];
+    return restaurant;
+}
+
+
 // flint options
 var config = {
     webhookUrl: 'http://b8f93335.ngrok.io/flint',
@@ -31,11 +39,12 @@ flint.hears('/hello', function(bot, trigger) {
     bot.say('Hello %s!', trigger.personDisplayName);
 });
 
+
 var Wagon = function (time, minPeople) {
     this.time = time;
     this.minPeople = minPeople;
     this.numPeople = 1;
-    this.restaurant = 'McDonalds';
+    this.restaurant = pickRandRestaurant();
     
     this.id = getId();
 
@@ -45,18 +54,25 @@ var Wagon = function (time, minPeople) {
     };*/
 
     console.log("create wagon " + this.time + " " + this.minPeople + " " + this.restaurant);
-}
+};
 
 
 flint.hears('/add', function(bot, trigger) {
     var restaurant = trigger.text.substring(13);
     console.log(restaurant);
+    restaurants.push(restaurant);
     bot.say('Added %s to the list of restaurants.', restaurant);
 });
 
 
 flint.hears('/food', function(bot, trigger) {
+    console.log('\/food');
     var keywords = trigger.text.split(" ");
+    if (keywords.length != 4) {
+        console.log('error');
+        bot.say('Invalid arguments. Please make sure the command is in the form \"\/food <time> <number of people>\"');
+        return;
+    } 
     var time = keywords[2];
     var numPeople = keywords[3];
     // TODO validation, convert time properly
@@ -65,7 +81,7 @@ flint.hears('/food', function(bot, trigger) {
     wagons[wagon.id] = wagon;
 
     bot.say('How about %s for lunch at %s? Looking for %s people.', wagon.restaurant, wagon.time, wagon.minPeople);
-    bot.say('Enter the command \"\\join %s\" to join this Foodwagon', wagon.id);
+    bot.say('Enter the command \"\/join %s\" to join this Foodwagon', wagon.id);
     
     console.log(wagons);
 });
