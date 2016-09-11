2d944d5c15c0ec04c35231b9fd712a8a0feff759
Jimmy Yan <yjimmyy@gmail.com>
2016-09-10 20:47:52 -0700

    add food and add options

--- index.js
+++ index.js
@@ -4,12 +4,21 @@ var Restify = require('restify');
 var server = Restify.createServer();
 server.use(Restify.bodyParser());
 
+
+var wagons = [];
+var wagonId = 1;
+function getId() {
+    return wagonId++;
+}
+
 // flint options
 var config = {
-  webhookUrl: 'http://myserver.com/flint',
-  token: 'Y2VmNDI4ZjQtMGU3My00YWE1LWE1NWItYzllNDlmMjIyNTMwMjY5ZjQzMjYtYzhi',
-  port: 80
+    webhookUrl: 'http://b8f93335.ngrok.io/flint',
+    token: 'NjExNDJlYWItZTllNC00MDMyLWE0YjItNDk3ZjBjZTcxODlkOWQ2MDVkZTItZDE3',
+    port: 80
 };
+
+server.get('/aaa', function() { console.log("aaa");});
 
 // init flint
 var flint = new Flint(config);
@@ -18,7 +27,47 @@ console.log('Started')
 
 // say hello
 flint.hears('/hello', function(bot, trigger) {
-  bot.say('Hello %s!', trigger.personDisplayName);
+    console.log("hello");
+    bot.say('Hello %s!', trigger.personDisplayName);
+});
+
+var Wagon = function (time, minPeople) {
+    this.time = time;
+    this.minPeople = minPeople;
+    this.numPeople = 1;
+    this.restaurant = 'McDonalds';
+    
+    this.id = getId();
+
+    /*this.addPerson = function(name) {
+        console.log("add " + name);
+        numPeople++;
+    };*/
+
+    console.log("create wagon " + this.time + " " + this.minPeople + " " + this.restaurant);
+}
+
+
+flint.hears('/add', function(bot, trigger) {
+    var restaurant = trigger.text.substring(13);
+    console.log(restaurant);
+    bot.say('Added %s to the list of restaurants.', restaurant);
+});
+
+
+flint.hears('/food', function(bot, trigger) {
+    var keywords = trigger.text.split(" ");
+    var time = keywords[2];
+    var numPeople = keywords[3];
+    // TODO validation, convert time properly
+
+    var wagon = new Wagon(time, numPeople);
+    wagons[wagon.id] = wagon;
+
+    bot.say('How about %s for lunch at %s? Looking for %s people.', wagon.restaurant, wagon.time, wagon.minPeople);
+    bot.say('Enter the command \"\\join %s\" to join this Foodwagon', wagon.id);
+    
+    console.log(wagons);
 });
 
 // define restify path for incoming webhooks
@@ -26,14 +75,15 @@ server.post('/flint', webhook(flint));
 
 // start restify server
 server.listen(config.port, function () {
-  flint.debug('Flint listening on port %s', config.port);
+    console.log("listening on port " + config.port);
+    flint.debug('Flint listening on port %s', config.port);
 });
 
 // gracefully shutdown (ctrl-c)
 process.on('SIGINT', function() {
-  flint.debug('stoppping...');
-  server.close();
-  flint.stop().then(function() {
-    process.exit();
-  });
+    flint.debug('stoppping...');
+    server.close();
+    flint.stop().then(function() {
+        process.exit();
+    });
 });
